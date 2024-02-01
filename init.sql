4742d3cb9289fcc4a0b7876d589d5b8e5635cfbd
Jordan Morillo <82860617+Jordan917222@users.noreply.github.com>
2024-02-01 15:04:49 -0500

    Init draft for sql
    Basic rough draft for what I think the 5 tables will be. Dont be afraid to change anything, this is just a starting point

---
+++ init.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE `databaseProj`; 
+USE `databaseProj`;
+-- Create database that will hold user credentials--
+CREATE TABLE `creds` (
+  `user_id` int(255) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `university` varchar(255),
+  `rso` varchar(255),
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- Create database that will hold event info--
+CREATE TABLE `events` (
+  `event_id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL AUTO_INCREMENT,
+  `category` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `time` time(50) NOT NULL, 
+  `date` date NOT NULL,
+  `location` varchar(255) NOT NULL, -- we have to set this with a google map or something?
+  `contact_phone` varchar(22) NOT NULL,
+  `contact_email` varchar(255) NOT NULL,
+  `rso` varchar(255), -- either relate to rso or be null/public
+  `university` varchar(255) NOT NULL, -- relate to university that created it
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Create database that will hold university--
+CREATE TABLE `university` (
+  `uni_id` int(255) NOT NULL,
+  `owner` varchar(255) NOT NULL AUTO_INCREMENT, -- will relate to the id of user that created the university?--
+  `location` varchar(255) NOT NULL,
+  `amt_students` varchar(255) NOT NULL,
+  -- we can add extra here, like contact info and such.
+PRIMARY KEY (`uni_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `comment` (
+  `comment_id` int(255) NOT NULL,
+  `event_id` varchar(255) NOT NULL AUTO_INCREMENT, -- This will relate to the event its posted on--
+  `content` varchar(255) NOT NULL,
+  `rating` int(255) NOT NULL,
+  `time` time(50) NOT NULL,  -- optional, date and time of comment creation?
+  `date` date NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `RSO` (
+  `rso_id` int(255) NOT NULL,
+  `rso` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL AUTO_INCREMENT, -- will relate to the id of user that created the rso maybe?--
+-- probably more info needed her
+PRIMARY KEY (`rso_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
